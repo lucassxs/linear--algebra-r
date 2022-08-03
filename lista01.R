f29598571bb4584dc55cb4207d147a8d1fbd767f
Lucas Stefano <lucassxs.sp@gmail.com>
2022-08-03 16:22:55 +0000

    fix gaussian elimination

--- lista01.R
+++ lista01.R
@@ -13,3 +13,23 @@ table(
     1 2 3 7
     4 5 6 2
 )
+
+## Eliminação Guassiana
+
+as.np(crosstabs <- ) # defino
+
+# Definindo a matrix
+A  <-  matrix(c(1,2,3,2,5,6,3,6,10), 3, 3)
+a
+Det(A)
+dET(A, verbose=TRUE, fractions=TRUE)
+
+#Definindo a matrix B
+B <- matrix(1:9, 3, 3) 
+Det(B, method="eigenvalues")
+Det(B, method="cofactors")
+
+echelon(A, B, reduced = TRUE, ...)
+EC$vectors %*% diag(EC$values) %*% t(EC$vectors)
+
+gaussianElimination()
